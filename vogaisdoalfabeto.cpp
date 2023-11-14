e43353bd05b8f7c4885c0cfaba39e308627b6738
Gustavo Mendes <gustavormendess@gmail.com>
2023-11-14 16:33:57 -0400

    Update vogaisdoalfabeto.cpp

--- vogaisdoalfabeto.cpp
+++ vogaisdoalfabeto.cpp
@@ -1,8 +1,8 @@
 #include <stdio.h>
 int main(void){
 	int contador;
-	char vetorAlfabeto[5] = {'a','e','i','o','u'};
+	char vogais[5] = {'a','e','i','o','u'};
 	for(contador = 0; contador <= 5; contador++){
-		printf("%c \n",vetorAlfabeto[contador]);
+		printf("%c \n",vogais[contador]);
 	}
 }
