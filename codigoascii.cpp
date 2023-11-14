09e8a16ec53b9976847b9f56ddcddd8118f0801e
Gustavo Mendes <gustavormendess@gmail.com>
2023-11-14 16:08:05 -0400

    Update codigoascii.cpp

--- codigoascii.cpp
+++ codigoascii.cpp
@@ -1,15 +1,15 @@
 #include <stdio.h>
-#include <ctype.h> // funcaoo isacii retorna um valor booleano
-//funcao toascii converte para ASCII
+#include <ctype.h> 
+// funcao isacii() retorna um valor booleano
+//funcao toascii() converte para código ASCII
  main(){
 	char caracter;
 	char toascii;
 	printf("Digite um caracter para saber seu codigo ASCII em octal, decimal e hexadecimal :\n");
 	scanf("%c",&caracter);		
 	if (isascii(caracter)) {
-    	printf("O codigo ASCII de %c e %d (octal), %d (decimal) e %x (hexadecimal).\n",
-         caracter, toascii(caracter), toascii(caracter), toascii(caracter));
-  	}	 else {
-    printf("O caracter %c nao e um caracter ASCII\n", caracter);
+    		printf("O codigo ASCII de %c e %d (octal), %d (decimal) e %x (hexadecimal).\n",caracter, toascii(caracter), toascii(caracter), toascii(caracter));
+  	}else {
+    		printf("O caracter %c nao e um caracter ASCII\n", caracter);
   	}
 }
