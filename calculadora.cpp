a59e192633496a0c0ad2a321d7f4aeb0d69ac902
Gustavo Mendes <gustavormendess@gmail.com>
2023-11-16 14:59:42 -0400

    Update calculadora.cpp

--- calculadora.cpp
+++ calculadora.cpp
@@ -1,36 +1,31 @@
 #include <stdio.h>
-//#include <stdlib.h>
-//#include <math.h>
-//#include <ctype.h>
-//#include <conio.h>
-main(){
-	char operacao;
-	double n1,n2,soma,sub,mul,div = 0;
-	printf("Escolha a operacao (+ - / *) : \n");
-	scanf("%c",&operacao);
-	printf("Digite o primeiro numero: \n");
-	scanf("%lf",&n1);
-	printf("Digite o segundo numero:\n");
-	scanf("%lf",&n2);
-		switch(operacao){
-		
-		case '+': 
-		soma = n1 + n2;
-		printf("A soma entre %.0lf + %.0lf = %.0lf",n1,n2,soma);
-		break;
-		case '-':
-			sub = n1 - n2;
-			printf("A subtracao entre %lf - %lf = %lf ",n1,n2,sub);
-			break;
-		case '*': 
-		mul = n1 * n2;
-		printf("A multiplicacao entre  %lf x %lf = %lf ",n1,n2,mul);
-		break;
-		case '/': 
-		div = n1 / n2;
-		printf("A divisao entre %lf / %lf = %lf",n1,n2,div);
-		break;
-		default:
-			printf("[ERRO] TENTE NOVAMENTE!");
-}
+int main(void){
+  char operacao;
+  double n1,n2,soma,sub,mul,div = 0;
+  printf("Escolha a operacao (+ - / *) : \n");
+  scanf("%c",&operacao);
+  printf("Digite o primeiro numero: \n");
+  scanf("%lf",&n1);
+  printf("Digite o segundo numero:\n");
+  scanf("%lf",&n2);
+  switch(operacao){	
+	case '+': 
+	  soma = n1 + n2;
+	  printf("A soma entre %.0lf + %.0lf = %.0lf",n1,n2,soma);
+	  break;
+	case '-':
+	  sub = n1 - n2;
+	  printf("A subtracao entre %lf - %lf = %lf ",n1,n2,sub);
+	  break;
+	case '*': 
+	  mul = n1 * n2;
+	  printf("A multiplicacao entre  %lf x %lf = %lf ",n1,n2,mul);
+	  break;
+	case '/': 
+	  div = n1 / n2;
+	  printf("A divisao entre %lf / %lf = %lf",n1,n2,div);
+	  break;
+	default:
+	  printf("[ERRO] TENTE NOVAMENTE!");
+  }
 }
